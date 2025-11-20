0b289a282665e65e6cab9d4a97088b654837bc1a
2marwa2 <marwa.alaa421@gmail.com>
2025-11-20 15:29:08 +0200

    Updating TestClass.java

--- src/test/java/testPackage/TestClass.java
+++ src/test/java/testPackage/TestClass.java
@@ -1,5 +1,8 @@
 package testPackage;
 
+import net.bytebuddy.asm.Advice;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
@@ -9,11 +12,19 @@ import java.net.URL;
 
 public class TestClass {
     @Test
-    public void test(){
+    public void a(){
         WebDriver driver = new ChromeDriver();
         driver.navigate().to("https://duckduckgo.com/");
         Assert.assertEquals(driver.getTitle(),"Google");
         driver.close();
 
 
-}}
+}
+@Test
+    public void c(){
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("https://duckduckgo.com/");
+        driver.findElement(By.id("searchbox_input")).sendKeys("Selenium WebDriver", Keys.ENTER);
+  driver.close();
+}
+}
